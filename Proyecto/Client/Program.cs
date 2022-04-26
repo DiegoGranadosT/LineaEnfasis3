ab1e81b7cd8b723f5a447c578d1b3753cab19156
DIEGO-GRANADOS\diego <diegocamilog@live.com>
2022-04-25 23:40:32 -0500

    .

--- Proyecto/Client/Program.cs
+++ Proyecto/Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using Proyecto.Client;
+using Proyecto.Client.JsInterop;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -14,5 +15,7 @@ builder.Services.AddMudServices();
 //builder.Services.AddScoped<CustomStateProvider>();
 //builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<CustomStateProvider>());
 //builder.Services.AddBlazoredModal();
+
+builder.Services.AddScoped<MqttJsInterop>();
 
 await builder.Build().RunAsync();
